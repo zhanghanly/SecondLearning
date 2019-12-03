cb5ebb9b136ea86fa2da95d4c4f95752a6bbdfad
root <15364934892@163.com>
2019-12-03 15:29:50 +0800

    Delete git warning message

--- main.cpp
+++ main.cpp
@@ -7,11 +7,8 @@ int main()
 	std::cout << "hello world" << std::endl;
 	sum(1, 2);
 	std::cout << "create a new branch which is named dev" << std::endl;
-<<<<<<< HEAD
 	std::cout << "master has a commit" << std::endl;
-=======
 	std::cout << "create a new branch which is named feature1" << std::endl;
 	std::cout << "solve confilict" << std::endl;
->>>>>>> feature1
 	return 0
 }
